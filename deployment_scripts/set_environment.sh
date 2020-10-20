e3dabb8a41ee49a69d2cb077df03e9299b4c2898
ramesh0812 <55582670+ramesh0812@users.noreply.github.com>
2020-10-20 13:01:16 +0100

    Update set_environment.sh

--- deployment_scripts/set_environment.sh
+++ deployment_scripts/set_environment.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 export DEFAULT_LOCATION=uksouth
-export SIRMIONE_ALIAS=xsirmione
-export LIMONE_ALIAS=xlimone
-export SCORPIO_ALIAS=xscorpio
-export VIRGO_ALIAS=xvirgo
-export LIBRA_ALIAS=xlibra
-export ZODIAC_ALIAS=xzodiac
-export DB_ADMIN_USER=nick
+export SIRMIONE_ALIAS=rd0108
+export LIMONE_ALIAS=rd0108
+export SCORPIO_ALIAS=rd0108
+export VIRGO_ALIAS=rd0108
+export LIBRA_ALIAS=rd0108
+export ZODIAC_ALIAS=rd0108
+export DB_ADMIN_USER=rameshadmin
 export AAD_DOMAIN=xekina.onmicrosoft.com
 export AAD_TENANTID=3bc03625-3a0a-48c5-8aa5-12f22e401fff
 export AAD_CLIENTID=ccee7608-940c-42d9-ba86-a2845ef3a808
